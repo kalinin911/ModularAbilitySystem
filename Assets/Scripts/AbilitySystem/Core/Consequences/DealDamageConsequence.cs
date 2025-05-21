7ddb1cd1955d01cba829021471bc52df24995394
Illia Kalinin <ikalinin911@gmail.com>
2025-05-21 15:10:02 +0200

    Changed DealDamangeConsequence to accept several targets

--- Assets/Scripts/AbilitySystem/Core/Consequences/DealDamageConsequence.cs
+++ Assets/Scripts/AbilitySystem/Core/Consequences/DealDamageConsequence.cs
@@ -1,11 +1,7 @@
-﻿using Assets.Scripts.Components.Character;
-using Cysharp.Threading.Tasks;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using System.Collections.Generic;
 using UnityEngine;
+using Cysharp.Threading.Tasks;
+using Assets.Scripts.Components.Character;
 
 namespace Assets.Scripts.AbilitySystem.Core.Consequences
 {
@@ -13,55 +9,47 @@ namespace Assets.Scripts.AbilitySystem.Core.Consequences
     public class DealDamageConsequence : Consequence
     {
         [SerializeField] private Stat _damageAmount;
-        [SerializeField] private TargetKey _targetKey;
+        [SerializeField] private TargetsListKey _targetKey;
         [SerializeField] private bool _showDamageText = true;
 
         protected override async UniTask ExecuteImplementationAsync(AbilityExecutionContext context)
         {
-            // Try to get the target from context
-            if (context.TryGetParameter<Transform>(_targetKey, out var target))
+            if (context.TryGetParameter<List<Transform>>(_targetKey, out var targets) && targets.Count > 0)
             {
-                // Try to apply damage to the target
-                var health = target.GetComponent<Health>();
-                if (health != null)
+                foreach (var target in targets)
                 {
-                    Debug.Log($"Dealing {_damageAmount.Value} damage to {target.name}");
-
-                    // Show damage text if enabled and effect service is available
-                    if (_showDamageText && context.EffectService != null)
-                    {
-                        context.EffectService.ShowDamageText(target.position, _damageAmount.Value);
-                    }
-
-                    health.TakeDamage(_damageAmount.Value);
-                }
-            }
-            else if (context.TryGetParameter<List<Transform>>(_targetKey, out var targets))
-            {
-                // If we got a list of targets instead
-                foreach (var t in targets)
-                {
-                    var health = t.GetComponent<Health>();
+                    var health = target.GetComponent<Health>();
                     if (health != null)
                     {
-                        Debug.Log($"Dealing {_damageAmount.Value} damage to {t.name}");
-
-                        // Show damage text if enabled and effect service is available
-                        if (_showDamageText && context.EffectService != null)
-                        {
-                            context.EffectService.ShowDamageText(t.position, _damageAmount.Value);
-                        }
-
+                        Debug.Log($"Dealing {_damageAmount.Value} damage to {target.name}");
                         health.TakeDamage(_damageAmount.Value);
                     }
                 }
             }
             else
             {
-                Debug.LogWarning($"DealDamageConsequence couldn't find target with key {_targetKey.name}");
+                Debug.LogWarning($"DealDamageConsequence couldn't find targets with key {_targetKey.name}");
             }
 
             await UniTask.CompletedTask;
+        }
+
+        private void ApplyDamageToTarget(Transform target)
+        {
+            // Try to apply damage to the target
+            var health = target.GetComponent<Health>();
+
+            if (health != null)
+            {
+                Debug.Log($"Dealing {_damageAmount.Value} damage to {target.name}");
+
+                // Apply the actual damage
+                health.TakeDamage(_damageAmount.Value);
+            }
+            else
+            {
+                Debug.LogWarning($"Target {target.name} has no Health component!");
+            }
         }
     }
 }
